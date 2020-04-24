6ba068ada37eda8224a0a1c3390f8b868a5dc694
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-24 02:05:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,1 +1,12 @@
-
+def my_each(array) # put argument(s) here
+  i = 0
+  while i < array.length do
+    yield (array[i])
+    i += 1
+  end
+  array
+end
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  i * i
+end
